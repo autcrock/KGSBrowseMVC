3ae7a6b29101d6efcb43d57eafd37ca75a38c479
Mike Thomas <mjt464@gmail.com>
2014-10-10 14:40:34 +0800

    Add Project files.

---
+++ KGSBrowseMVC/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KGSBrowseMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload()
+        {
+            return null;
+            //return View();
+        }
+
+        public ActionResult Uploadfiles()
+        {
+            // Can pass a model in here as an argument to View
+            return View();
+        }
+    }
+}
---
+++ KGSBrowseMVC/Models/LAStoJavascript.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Web.UI;
+
+namespace KGSBrowseMVC.Models
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //DisplayCodeLocation.Text = "Hello, world!";
+        }
+    }
+}
